0c4fc69b09c3b066c94bbe2322869d2a7a07f608
Alix Vaillancourt <83536737+alixvaillancourt@users.noreply.github.com>
2021-05-05 16:21:59 -0400

    les trois porte v1.4
    ajout d'un system de vies

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let cP;
 let score = 0;
+let vie = 3;
+
+const pv1 = document.getElementById("pv1");
+const pv2 = document.getElementById("pv2");
+const pv3 = document.getElementById("pv3");
+
+pv1.style.display = "block";
+pv2.style.display = "block";
+pv3.style.display = "block";
 
 const porte1 = document.getElementById("porte1").addEventListener("click", function(){
     cP = 0;
@@ -19,9 +28,18 @@ const porte3 = document.getElementById("porte3").addEventListener("click", funct
 const porte4 = document.getElementById("porte4");
 porte4.addEventListener("click", function () {
     porte4.style.display = "none";
-    alert("UN FANTOME!!! Tu as perdu, ton score est de: " + score + "!");
-    score = 0;
-    document.getElementById("score").innerHTML = "score = " + score;
+    if (vie == 0) {
+        alert("UN FANTOME!!! Tu as perdu, ton score est de: " + score + "!");
+        score = 0;
+        document.getElementById("score").innerHTML = "score = " + score;
+        vie = 3;
+        pv1.style.display = "block";
+        pv2.style.display = "block";
+        pv3.style.display = "block";
+    }else{
+        alert("UN FANTOME!!! Il te reste " + vie + " vie. Continu ton chemin...");
+    }
+
 });
 const porte5 = document.getElementById("porte5");
 porte5.addEventListener("click", function (params) {
@@ -35,6 +53,16 @@ function verif() {
     quelPorte();
     if(cP === quelPorte()){
         porte4.style.display = "block";
+        vie--;
+        if (vie == 2) {
+            pv3.style.display = "none";
+        }else if (vie == 1) {
+            pv2.style.display = "none";
+        }else if (vie == 0) {
+            pv1.style.display = "none";
+        }{
+
+        }
     }else{
         porte5.style.display = "block";
         score++;
